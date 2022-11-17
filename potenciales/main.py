25144dcca57bcdf99acd56c5b46637457b603e53
MateoBarrera <materile1997@gmail.com>
2022-11-17 07:52:58 -0500

    Iniciamos investigción

---
+++ potenciales/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+#file = pd.read_csv('./recrusos/global/D1_POWER_Point_Monthly_Timeseries_2011_2021_LST.csv')
+file = pd.read_csv(
+    './recrusos/hydro/caudal_medio_mensual/excel.csv.csv')
+print(file.head())
---
+++ potenciales/potenciales.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class Sources():
+  def __init__(self) -> None:
+    self.name
+    self.description
+
+class Resources():
+  def __init__(self) -> None:
+    self.name_resource
+    self.database
+
+class GraphSource(Sources):
+  pass
